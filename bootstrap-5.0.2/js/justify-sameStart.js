e946cb0e737590ba4d8cef240aefccc7ff45ce0a
Dilan Joel Bergano Caicedo <=>
2022-03-24 01:08:15 -0500

    First commit

---
+++ bootstrap-5.0.2/js/justify-sameStart.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+  function obtenerElementos() {
+    const elementosDerechos = document.getElementsByClassName("#elemento-der");
+    const elementosIzquierdos =
+      document.getElementsByClassName("#elemento-izq");
+    console.log("ELEMENTOS IZQ: ", elementosIzquierdos);
+  }
+
+
